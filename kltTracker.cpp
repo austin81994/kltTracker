c07d993f761a30b41b09d8508d8f57776eae15c4
austin81994 <37091746+austin81994@users.noreply.github.com>
2018-05-14 20:54:44 -0700

    Update kltTracker.cpp

--- kltTracker.cpp
+++ kltTracker.cpp
@@ -17,19 +17,19 @@ bool initializeFeatureTracking = false;
 //default capture width and height
 const int FRAME_WIDTH = 640;
 const int FRAME_HEIGHT = 480;
-const int MAX_COUNT = 50;
-
-static void onMouse(int, int, int, int /*flags*/, void* /*param*/);
+const int MAX_COUNT = 15;
 
 int main()
 {
-    //VideoCapture capture;
+	//**********Swap the file path to switch between the Drone tracking video or Thermal Video
 	string filePath = "bebop_images/frame";
+	//string filePath = "robot_thermal_data/tau2_fog_";
 	string fileExtension = ".jpg";
-	int frameIndex = 0;
+	int frameIndex = 1;
 	stringstream frameFile;
 	//stringstream stringFrameIndex;
 
+	VideoCapture capture;
     Mat cameraFrame, grayFrame, grayFrameOld;
     //Mat mask;
     Rect2d rect;
@@ -38,7 +38,6 @@ int main()
     TermCriteria termcrit(TermCriteria::COUNT|TermCriteria::EPS,20,0.03);
     vector<Point2f> pointsOld;
     vector<Point2f> pointsNew;
-    //Point2f centerOfFeatureTrackingArea;
     Size subPixWinSize(10,10);
     Size subPixZeroZone(-1,-1);
     Size winSizeForOpticalFlow(31,31);
@@ -46,28 +45,28 @@ int main()
     vector<uchar> status;
     vector<float> err;
     namedWindow( "KLT Tracker", 1 );
-    setMouseCallback("KLT Tracker", onMouse, 0);
 
-    //capture.open(0);
-    //capture.set(CV_CAP_PROP_FRAME_WIDTH,FRAME_WIDTH);
-    //capture.set(CV_CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT);
+    capture.open(0);
+    capture.set(CV_CAP_PROP_FRAME_WIDTH,FRAME_WIDTH);
+    capture.set(CV_CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT);
 
-    //capture.read(cameraFrame);
-    //cameraFrame.copyTo(grayFrame);
-    //cvtColor(cameraFrame, grayFrame, COLOR_BGR2GRAY);
+    capture.read(cameraFrame);
+    cameraFrame.copyTo(grayFrame);
+    cvtColor(cameraFrame, grayFrame, COLOR_BGR2GRAY);
 
     while(1)
     {
     	if(!pauseVideo)
     	{
-	        //capture.read(cameraFrame);
-	        //cvtColor(cameraFrame, grayFrame, COLOR_BGR2GRAY);
-	    	frameFile << filePath << setw(4) << setfill('0') << frameIndex << fileExtension; //build string file path/name of the image frame
-	    	//cout << frameFile.str() << endl;
+    		//**********Various lines below need to be commented/uncommented to get image frames from camera or from file input
+	    	frameFile << filePath << setw(4) << setfill('0') << frameIndex << fileExtension; //build string file path to drone file
+	    	//frameFile << filePath << frameIndex << fileExtension; //build string file path to thermal file
 	    	frameIndex = (frameIndex + 1) % 451;//increment image frame number
-	    	cameraFrame = imread(frameFile.str(), CV_LOAD_IMAGE_COLOR);
-	    	cvtColor(cameraFrame, grayFrame, COLOR_BGR2GRAY);
+	    	//frameIndex = (frameIndex + 1) % 4974;//increment image frame number
 	    	frameFile.str("");
+	    	capture.read(cameraFrame);
+	    	//cameraFrame = imread(frameFile.str(), CV_LOAD_IMAGE_COLOR);
+    		cvtColor(cameraFrame, grayFrame, COLOR_BGR2GRAY);
 
 	        if(!pointsNew.empty())
 	        {
@@ -87,10 +86,10 @@ int main()
 										status, /*Status Vector - T/F if flow for corresponding corner has been found*/
 										err, /*Error Vector - */
 										winSizeForOpticalFlow, /*size of the search window at each pyramid level*/
-										5, /*pyramid level number*/
+										6, /*pyramid level number*/
 										termcrit, 
 										0, 
-										0.000001); /*minimum eigen value of 2x2 matrix. sets a standard (lower = more strict) for filtering out bad points.*/
+										0.0000001); /*minimum eigen value of 2x2 matrix. sets a standard (lower = more strict) for filtering out bad points.*/
 	        }
 
 	    }
@@ -100,7 +99,6 @@ int main()
         	cvtColor(cameraFrame, grayFrame, COLOR_BGR2GRAY);
         	imshow("KLT Tracker", cameraFrame);//refresh the image so we draw the roi in the most recent frame
         	pointsOld.clear();//we are grabbing new points so all old points will be irrelevant
-        	//grayFrameOld.release();
         	grayFrame.copyTo(grayFrameOld);
         	rect = selectROI("KLT Tracker", cameraFrame);//roi selection box to grab the object of interest
         	Mat mask = Mat::zeros(grayFrame.size(), CV_8U);//declare matrix of same size as our frame
@@ -109,18 +107,16 @@ int main()
             goodFeaturesToTrack(	grayFrame, 
             						pointsNew, 
             						MAX_COUNT, /*max number of corners*/
-            						0.2, /*corner quality*/ 
-            						3, /*min distance between corners*/
+            						0.35, /*corner quality*/ 
+            						5, /*min distance between corners*/
             						mask, /*roi mask*/
             						3, /*block size*/
             						//3, /*unknown variable found in examples?*/
-            						true, /*use Harris Detector*/
+            						false, /*use Harris Detector*/
             						0.1); /*parameter for Harris Detector*/
-            //pointsNew.push_back(centerOfFeatureTrackingArea);
             cornerSubPix(grayFrame, pointsNew, subPixWinSize, subPixZeroZone, termcrit);
             pointsOld = pointsNew;
             initializeFeatureTracking = false;
-            //centerOfFeatureTrackingArea = Point2f(-1,-1);
         }
         for(int i = 0; i < pointsNew.size(); i++)
 		{
@@ -128,7 +124,7 @@ int main()
 	    }
 	    imshow("KLT Tracker", cameraFrame);
 
-        char c = (char)waitKey(60);
+        char c = (char)waitKey(30);
         if(c == 27)
             break;
         switch(c)
@@ -141,25 +137,10 @@ int main()
             pointsNew.clear();
             break;
         case 's'://initialize feature tracking
-        	//pauseVideo = !pauseVideo;
         	initializeFeatureTracking = true;
             break;
-
-        // case 'n':
-        //     nightMode = !nightMode;
-        //     break;
         }
 		swap(pointsNew, pointsOld);
 		swap(grayFrameOld, grayFrame);
     }
-}
-
-static void onMouse( int event, int x, int y, int /*flags*/, void* /*param*/ )
-{
-	if( event == EVENT_LBUTTONDOWN )
-	{
-   		//cout << "mouse click" << endl;
-		//centerOfFeatureTrackingArea = Point2f((float)x, (float)y);
-		//initializeFeatureTracking = true;
-	}
 }
